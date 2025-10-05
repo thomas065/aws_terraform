4051dec577904283073141c5a959009ca2ddd459
Thomas Bell <thomasjbell065@gmail.com>
2025-10-05 15:47:18 -0400

    updated

--- 1.2-EC2_instance.tf
+++ 1.2-EC2_instance.tf
@@ -6,52 +6,8 @@ resource "aws_instance" "web" {
   instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.public-eu-west-1a.id
 
-  #  user_data = base64encode(templatefile("${path.module}/templates/instance_tf.tpl"), {})
-
-  user_data = base64encode(<<-EOF
-    #!/bin/bash
-    yum update -y
-    yum install -y httpd
-    systemctl start httpd
-    systemctl enable httpd
-
-    # Get the IMDSv2 token
-    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-
-    # Background the curl requests
-    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/local-ipv4 &> /tmp/local_ipv4 &
-    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone &> /tmp/az &
-    curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ &> /tmp/macid &
-    wait
-
-    macid=$(cat /tmp/macid)
-    local_ipv4=$(cat /tmp/local_ipv4)
-    az=$(cat /tmp/az)
-    vpc=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$macid/vpc-id)
-
-    # Create HTML file
-    cat <<-HTML > /var/www/html/index.html
-    <!doctype html>
-    <html lang="en" class="h-100">
-    <head>
-    <title>Details for EC2 instance</title>
-    </head>
-    <body>
-    <div>
-    <h1>Team Sao Paulo</h1>
-    <h1>Chains Broken in Massachusetts</h1>
-    <p><b>Instance Name:</b> $(hostname -f) </p>
-    <p><b>Instance Private Ip Address: </b> $local_ipv4</p>
-    <p><b>Availability Zone: </b> $az</p>
-    <p><b>Virtual Private Cloud (VPC):</b> $vpc</p>
-    </div>
-    </body>
-    </html>
-    HTML
-
-    # Clean up the temp files
-    rm -f /tmp/local_ipv4 /tmp/az /tmp/macid
-  EOF
+  user_data = base64encode(
+    templatefile("${path.module}/templates/instance_tf.tpl", {})
   )
 
   root_block_device {
@@ -63,4 +19,8 @@ resource "aws_instance" "web" {
   tags = {
     Name = "Ubuntu"
   }
+}
+
+output "endpoint" {
+  value = "${aws_instance.web.public_ip}:80"
 }
