36cb46979ef757ed99c905ddef07fcafd9f3bdf9
Thomas Bell <thomasjbell065@gmail.com>
2025-10-05 14:19:50 -0400

    separated ec2 file

---
+++ 1.2-EC2_instance.tf
@@ -0,0 +1,18 @@
+# EC2-Instance
+
+resource "aws_instance" "web" {
+  ami                         = "ami-0bed3b2519c0c407f" # Replace with your desired AMI ID (https://cloud-images.ubuntu.com/locator/ec2/)
+  associate_public_ip_address = true
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public-eu-west-1a.id
+
+  root_block_device {
+    delete_on_termination = true
+    # volume_size           = 10
+    # volume_type           = "gp3"
+  }
+
+  tags = {
+    Name = "Ubuntu"
+  }
+}
